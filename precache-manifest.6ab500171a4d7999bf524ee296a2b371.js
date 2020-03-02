d1f952d4c703e2c43ccc23df8fadee549ac58060
Maxim Pavlenko <maxim@MacBook-Air.local>
2020-03-02 16:56:53 +0200

    Updates

---
+++ precache-manifest.6ab500171a4d7999bf524ee296a2b371.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15f15dc35ea4891e298caee9c0510cf8",
+    "url": "/react-hw-2-phonebook/index.html"
+  },
+  {
+    "revision": "b74d66f76ea102e204f3",
+    "url": "/react-hw-2-phonebook/static/css/main.d524d3b1.chunk.css"
+  },
+  {
+    "revision": "7857410486a21b13f937",
+    "url": "/react-hw-2-phonebook/static/js/2.d9ef2c2c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-hw-2-phonebook/static/js/2.d9ef2c2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b74d66f76ea102e204f3",
+    "url": "/react-hw-2-phonebook/static/js/main.c6220f30.chunk.js"
+  },
+  {
+    "revision": "9a436021446b28bdac67",
+    "url": "/react-hw-2-phonebook/static/js/runtime-main.448c8bdb.js"
+  }
+]);
